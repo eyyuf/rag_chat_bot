6769bc8fc87562491890e7c7885595e998258dfe
Eyyu <eyufeyisa7@gmail.com>
2026-02-14 22:08:47 +0300

    wouldnt work so trying to do in another directory an bringing it here again

--- backend/src/config/voyage.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-    apiKey: process.env.VOYAGER_AI_API_KEY,
-    model: "voyage-3",
-    baseUrl: "https://api.voyageai.com/v1/embeddings"
-};
--- backend/src/utils/logger.js
+++
@@ -1,8 +0,0 @@
-const logger = {
-    info: (msg) => console.log(`[INFO] ${msg}`),
-    error: (msg, err) => console.error(`[ERROR] ${msg}`, err || ""),
-    warn: (msg) => console.warn(`[WARN] ${msg}`),
-    debug: (msg) => process.env.NODE_ENV === 'development' && console.log(`[DEBUG] ${msg}`)
-};
-
-module.exports = logger;
